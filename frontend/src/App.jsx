0aa0bc7eb105c089e8dcee7271d0308d6f4c0ab7
Moises Guerola <daw.moisesguerola@gmail.com>
2023-01-20 22:37:58 +0100

    React Proyect Created & Tailwind Installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Layout/HeaderComponent'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className='bg-[#F3F4FD] dark:bg-[#121212] dark:text-[#f3f4fd] w-screen h-screen'>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  )
+}
---
+++ frontend/src/components/Layout/HeaderComponent.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <h1>INSIDE</h1>
+    )
+}
